7bb776a51d54e621ca3e3c4172f8474c24b5a951
a <test@com>
2019-12-10 18:00:54 +0900

    commit : construting the spring security login with remember me features

---
+++ spring-security/spring-security-002-login-remember-me/src/main/java/com/kyiminhan/spring/App.java
@@ -0,0 +1,28 @@
+package com.kyiminhan.spring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@EnableJpaAuditing
+@EnableJpaRepositories
+@SpringBootApplication
+@ComponentScan(basePackages = "com.kyiminhan")
+@EntityScan(basePackages = { "com.kyiminhan.spring.entity" })
+public class App implements CommandLineRunner {
+
+	public static void main(String... args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ spring-security/spring-security-002-login-remember-me/src/main/java/com/kyiminhan/spring/config/SecurityConfig.java
@@ -0,0 +1,103 @@
+package com.kyiminhan.spring.config;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.NonNull;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	@Qualifier("loginServiceImpl")
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private SuccessHandlar successHandlar;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/login").permitAll();
+		http.authorizeRequests().antMatchers("/**").hasAnyRole("USER", "MANAGER", "ADMIN");
+		http.authorizeRequests().antMatchers("/user/**").hasAnyRole("USER");
+		http.authorizeRequests().antMatchers("/manager/**").hasAnyRole("MANAGER");
+		http.authorizeRequests().antMatchers("/admin/**").hasAnyRole("ADMIN");
+		// @formatter:off
+		http
+		.formLogin()
+		.loginPage("/login")
+		.loginProcessingUrl("/login")
+		.successHandler(this.successHandlar)
+		.usernameParameter("email")
+		.passwordParameter("password")
+		.permitAll();
+		// @formatter:on
+
+		super.configure(http);
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		final DaoAuthenticationProvider provider = new DaoAuthenticationProvider() {
+			@Override
+			public Authentication authenticate(final Authentication authentication) throws AuthenticationException {
+				if (StringUtils.isBlank(authentication.getName())) {
+					throw new UsernameNotFoundException("Email is required.");
+				} else if (!ObjectUtils.anyNotNull(authentication.getCredentials())
+						| StringUtils.isBlank(authentication.getCredentials().toString())) {
+					throw new BadCredentialsException("Password is required.");
+				} else {
+					try {
+						return super.authenticate(authentication);
+					} catch (final BadCredentialsException e) {
+						throw new BadCredentialsException("Invalid email and password.");
+					}
+				}
+			}
+
+			@Override
+			public void setPasswordEncoder(final PasswordEncoder passwordEncoder) {
+				super.setPasswordEncoder(SecurityConfig.this.getPasswordEncoder());
+			}
+		};
+		provider.setUserDetailsService((@NonNull final String email) -> {
+			final UserDetails userDetails = SecurityConfig.this.userDetailsService.loadUserByUsername(email);
+			if (!ObjectUtils.anyNotNull(userDetails)) {
+				throw new UsernameNotFoundException("User Not Found");
+			}
+			return userDetails;
+		});
+		auth.authenticationProvider(provider);
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/static/css/**", "/static/js/**");
+	}
+
+}
