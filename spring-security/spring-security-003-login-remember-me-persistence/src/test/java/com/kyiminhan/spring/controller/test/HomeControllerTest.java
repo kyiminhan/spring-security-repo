d05b83b4831d8828ff6348e5b836e40bc4e7f619
a <test@com>
2019-12-12 13:27:31 +0900

    commit : creating the spring security login with remember me persistent_logins table.

---
+++ spring-security/spring-security-003-login-remember-me-persistence/src/test/java/com/kyiminhan/spring/controller/test/HomeControllerTest.java
@@ -0,0 +1,5 @@
+package com.kyiminhan.spring.controller.test;
+
+public class HomeControllerTest {
+
+}
---
+++ spring-security/spring-security-entity/src/main/java/com/kyiminhan/spring/entity/PersistentLogins.java
@@ -0,0 +1,20 @@
+package com.kyiminhan.spring.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "persistent_logins")
+public class PersistentLogins {
+
+	private String username;
+}
