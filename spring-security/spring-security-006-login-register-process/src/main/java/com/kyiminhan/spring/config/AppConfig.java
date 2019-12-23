ceffa60e39c278e53c65f122e0cc461da9876a6b
kyiminhan <kyiminhan@localhost>
2019-12-24 03:24:51 +0900

    commit : adding the message validator factory bean.

--- spring-security/spring-security-006-login-register-process/src/main/java/com/kyiminhan/spring/config/AppConfig.java
+++ spring-security/spring-security-006-login-register-process/src/main/java/com/kyiminhan/spring/config/AppConfig.java
@@ -6,9 +6,17 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 public class AppConfig {
+
+	@Bean
+	public LocalValidatorFactoryBean getValidator() {
+		final LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(this.messageSource());
+		return bean;
+	}
 
 	@Bean
 	public MessageSource messageSource() {
